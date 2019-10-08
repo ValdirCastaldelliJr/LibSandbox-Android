aa9a7d1996870aab320402c57f0b4713626c335f
Valdir Castaldelli Junior <valdir.castaldelli@gmail.com>
2019-10-08 10:57:22 -0300

    Initial code commit

---
+++ app/src/main/java/com/castaldelli/libsandbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.castaldelli.libsandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
